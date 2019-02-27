7464247d8f71ccf760b975c572a6fac14caddcf2
Jakub Ronkiewicz <jronkiewicz@sigma.ug.edu.pl>
2019-02-27 13:39:30 +0100

    Some improvements

---
+++ zero_jadro/README.md
@@ -0,0 +1,15 @@
+1. Wykryj wszystkie minima lokalne
+2. Wykryj wszystkie maksima lokalne
+
+! Sciezka nie moze sie wracac
+
+# Inicjalizacja
+Maksimum najwyzszy, minmium najnizszy
+Jesli sie zmieni to znaczy, ze istnieje 
+
+
+Jedna petla szukam minima
+Druga petla szukam maksima
+Porownuje wartosci 
+
+Punkt przeciecia dwoch prostych
--- zero_jadro/main.py
+++ zero_jadro/main.py
@@ -1,8 +1,30 @@
 from matplotlib import pyplot as plt
-from shapely.geometry.polygon import Polygon
+from shapely.geometry.polygon import Polygon, orient
+from shapely.geometry import mapping, shape
 from utility import Utility
+import json
 
 util = Utility()
+
+
+def from_json(filename='data.json'):
+    with open(filename, 'r') as f:
+        data = json.loads(f.read())
+    return shape(data)
+
+def find_min_max(polygon):
+    #     _
+    # /\ | | - kolec dolny; \/ |_| - kolec gorny
+
+    coordinates = util.coords_as_list(polygon)
+    for coord in coordinates:
+        print(coord[0], coord[1])
+
+    # only x 
+    xaxis = list(zip(*coordinates))[0]
+    for x in xaxis:
+        print(x)
+
 
 poly = Polygon([
     (0,0),
@@ -13,15 +35,11 @@ poly = Polygon([
     (1,0.8),
 ])
 
-triangle = Polygon([
-    (0,0),
-    (0,3),
-    (4,0)
-])
-
+triangle = from_json()
 
 util.draw(*poly.exterior.xy)
 util.draw(*triangle.exterior.xy)
 
-print(util.coords_as_list(poly))
+find_min_max(triangle)
+
 util.show()
